d78e54500341ca2121fc450ae5f822ddd41cd005
Paul Adams <pauladams12345@gmail.com>
2019-10-25 22:56:18 -0400

    Use base url "/" for both login and homepage

--- routes/index.js
+++ routes/index.js
@@ -7,51 +7,55 @@ var express = 	require('express'),
 // router.use('/preferences', require('./preferences'));
 
 // Route for landing page
-router.get('/login', function(req, res) {
-	var context = {};
-	context.layout = 'no_navbar.handlebars'
-	context.stylesheets = ['main.css', 'login.css']
-	res.render('login', context);
-});
+// router.get('/login', function(req, res) {
+// 	var context = {};
+// 	context.layout = 'no_navbar.handlebars'
+// 	context.stylesheets = ['main.css', 'login.css']
+// 	res.render('login', context);
+// });
 
 router.get('/', function(req, res) {
 	var context = {};
-	var validation_ticket = req.query.ticket;
-	console.log('Ticket: ' + validation_ticket);
 
-	var options = {
-		method: 'GET',
-		uri: 'https://login.oregonstate.edu/idp/profile/cas/serviceValidate',
-		headers: {
-			'Content-Type': 'text/xml'
-		},
-		qs: {
-			ticket: validation_ticket,
-			service: 'https://indaba-scheduler.herokuapp.com/'
-		}
-	};
+	// If there's no CAS ticket in the query string, render the landing page
+	if (!req.query.ticket) {
+		context.layout = 'no_navbar.handlebars'
+		context.stylesheets = ['main.css', 'login.css']
+		res.render('login', context);
+	}
 
-	request(options, function(err, res, body) {
-		console.log('BODY:\n\n\n' + body);
-		var json = JSON.parse(parser.toJson(body));
-		str = JSON.stringify(json);
-		console.log(str);
-		var attributes = json['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
-		str2 = JSON.stringify(attributes);
-		console.log(str2);
-		var onid = attributes['cas:uid'];
-		var firstName = attributes['cas:firstname'];
-		var lastName = attributes['cas:lastname'];
-		var fullName = attributes['cas:fullname'];
-		var email = attributes['cas:email'];
-		console.log(onid);
-		console.log(firstName);
-		console.log(lastName);
-		console.log(fullName);
-		console.log(email);
-	});
+	// Otherwise, user has been redirected from CAS, render personal homepage
+	else {
+		// Get ticket ID
+		var validation_ticket = req.query.ticket;
 
-	res.render('home', context);
+		// Set up ticket validation request to CAS
+		var options = {
+			method: 'GET',
+			uri: 'https://login.oregonstate.edu/idp/profile/cas/serviceValidate',
+			headers: {
+				'Content-Type': 'text/xml'
+			},
+			qs: {
+				ticket: validation_ticket,
+				service: 'https://indaba-scheduler.herokuapp.com/'
+			}
+		};
+
+		// Send validation request
+		request(options, function(err, res, body) {
+			// Parse successful request
+			var json = JSON.parse(parser.toJson(body));
+			var attributes = json['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
+			var onid = attributes['cas:uid'];
+			var firstName = attributes['cas:firstname'];
+			var lastName = attributes['cas:lastname'];
+			var fullName = attributes['cas:fullname'];
+			var email = attributes['cas:email'];
+		});
+
+		res.render('home', context);
+	}
 })
 
 module.exports = router;
