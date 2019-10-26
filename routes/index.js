748ae87a1b4148ccf3757254cc23ac1f4235725b
Everett Williams <everettwilliams@localhost.localdomain>
2019-10-26 16:13:27 -0400

    added router.get for retrieving users in test_OSU_Users

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = 	require('express'),
 	router = 	express.Router(),
 	parser =	require('xml2json');
+var mysql = require('../middleware/dbcon.js');
 
 // Display landing page or authenticate user and redirect
 router.get('/', function(req, res) {
@@ -51,8 +52,20 @@ router.get('/', function(req, res) {
 		//TODO: change to a redirect instead of a render
 		let context = {};
 		context.stylesheets = ['main.css', 'home.css'];
-		res.render('home');	
+		res.render('home');
 	}
 })
+
+app.get('/test_OSU_Users',function(req,res,next){
+  var context = {};
+  mysql.pool.query('SELECT * FROM indaba_db.OSU_member', function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = JSON.stringify(rows);
+    res.render(context.results);
+  });
+});
 
 module.exports = router;
