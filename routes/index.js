2be05b749a41d9e0a24ae76259b4ce3253d2b1e8
Paul Adams <pauladams12345@gmail.com>
2019-10-25 23:05:42 -0400

    Clean up, add comments

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,25 @@ var express = 	require('express'),
 	router = 	express.Router(),
 	parser =	require('xml2json');
 
-// For pages other than the landing page, include a line
-// like the following:
-// router.use('/preferences', require('./preferences'));
-
-// Route for landing page
-// router.get('/login', function(req, res) {
-// 	var context = {};
-// 	context.layout = 'no_navbar.handlebars'
-// 	context.stylesheets = ['main.css', 'login.css']
-// 	res.render('login', context);
-// });
-
+// Display landing page or authenticate user and redirect
 router.get('/', function(req, res) {
-	var context = {};
 
 	// If there's no CAS ticket in the query string, render the landing page
-	if (!req.query.ticket) {
+	if (!req.query.ticket) {	//TODO: change this logic to look for a
+		let context = {}
 		context.layout = 'no_navbar.handlebars'
 		context.stylesheets = ['main.css', 'login.css']
 		res.render('login', context);
 	}
 
-	// Otherwise, user has been redirected from CAS, render personal homepage
+	// Otherwise, user has been redirected from CAS
+	// Validate ticket, redirect to personal homepage
 	else {
 		// Get ticket ID
-		var validation_ticket = req.query.ticket;
+		let validation_ticket = req.query.ticket;
 
 		// Set up ticket validation request to CAS
-		var options = {
+		let options = {
 			method: 'GET',
 			uri: 'https://login.oregonstate.edu/idp/profile/cas/serviceValidate',
 			headers: {
@@ -44,17 +34,21 @@ router.get('/', function(req, res) {
 
 		// Send validation request
 		request(options, function(err, res, body) {
+			//TODO: add error handling
 			// Parse successful request
-			var json = JSON.parse(parser.toJson(body));
-			var attributes = json['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
-			var onid = attributes['cas:uid'];
-			var firstName = attributes['cas:firstname'];
-			var lastName = attributes['cas:lastname'];
-			var fullName = attributes['cas:fullname'];
-			var email = attributes['cas:email'];
+			let json = JSON.parse(parser.toJson(body));
+			let attributes = json['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
+			let onid = attributes['cas:uid'];
+			let firstName = attributes['cas:firstname'];
+			let lastName = attributes['cas:lastname'];
+			let fullName = attributes['cas:fullname'];
+			let email = attributes['cas:email'];
 		});
 
-		res.render('home', context);
+		//TODO: change to a redirect instead of a render
+		let context = {};
+		context.stylesheets = ['main.css', 'home.css'];
+		res.render('home');	
 	}
 })
 
