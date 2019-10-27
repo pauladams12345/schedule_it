0b4b0c6e7d2bc6e49ea377849b4411018c6c5fd5
Everett Williams <everettwilliams@localhost.localdomain>
2019-10-26 21:10:07 -0400

    testing database editions 13

--- middleware/dbcon.js
+++ middleware/dbcon.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
   host            : 'indaba-1.c0ib6xevgq1s.us-east-1.rds.amazonaws.com',
   user            : 'indabaAdmin',
   password        : 'abadni619',
-  database        : 'indaba-1'
+  database        : 'indaba-1',
   PORT            : '3306'
 });
 
