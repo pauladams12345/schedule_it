d65a9b35e67dc213ea5e2b84e0974e199358b2d8
Everett Williams <everettwilliams@localhost.localdomain>
2019-10-27 10:56:42 -0400

    testing database editions 19

--- routes/index.js
+++ routes/index.js
@@ -58,15 +58,15 @@ router.get('/', function(req, res) {
 
 router.get('/test_OSU_Users',function(req, res){
   var context = {};
-  /*mysql.pool.query('SELECT * FROM indaba_db.OSU_member', function(err, rows, fields){
+  mysql.pool.query('SELECT * FROM indaba_db.OSU_member', function(err, rows, fields){
     if(err){
       next(err);
       return;
     }
     context.results = JSON.stringify(rows);
     res.render('test_OSU_Users');
-  });*/
-	res.render('test_OSU_Users');
+  });
+	//res.render('test_OSU_Users');
 });
 
 module.exports = router;
