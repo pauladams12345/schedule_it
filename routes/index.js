df95c57b89346576eef10c19fab3de5944d2f1ac
pauladams12345 <pauladams12345@gmail.com>
2019-10-29 17:44:15 -0400

    Change request to promise

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = 	require('express'),
 	router = 	express.Router(),
 	parser =	require('xml2json'),
 	mysql = 	require('../middleware/dbcon.js'),
-	request = 	require('request'),
+	rp = 		require('request-promise-native'),
 	session = 	require('express-session');
 
 // Display landing page or authenticate user and redirect
@@ -36,55 +36,52 @@ router.get('/', function(req, res, next) {
 		};
 		//TODO: Break code below up into functions, maybe a module?
 		// Send validation request
-		request(options, function(err, response, body) {
-			//TODO: add error handling
-			// Parse successful request
-			let json = JSON.parse(parser.toJson(body));
-			let attributes = json['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
-			let onid = attributes['cas:uid'];
-			let firstName = attributes['cas:firstname'];
-			let lastName = attributes['cas:lastname'];
-			let fullName = attributes['cas:fullname'];
-			let email = attributes['cas:email'];
+		rp(options)
+			.then(function (body) {
+				//TODO: add error handling
+				// Parse successful request
+				let json = JSON.parse(parser.toJson(body));
+				let attributes = json['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
+				let onid = attributes['cas:uid'];
+				let firstName = attributes['cas:firstname'];
+				let lastName = attributes['cas:lastname'];
+				let fullName = attributes['cas:fullname'];
+				let email = attributes['cas:email'];
 
-			//check if user with this onid exists
-			mysql.pool.query("SELECT * FROM `OSU_member` WHERE onid = '" + onid + "'", function(err, result){
-				if(err){
-					next(err);
-					return;
-				}
-				// if yes, log them in
-				else if (result.length > 0) {
-					session.onid = result[0].onid;
-					session.firstName = result[0].first_name;
-				}
-				//if no, add them to the database
-				else {
-					mysql.pool.query("INSERT INTO indaba_db.OSU_member (`onid`,`first_name`,`last_name`,`ONID_email`) VALUES (?,?,?,?)",
-					  [onid, firstName, lastName, email], function(err, result){
-						if(err){
-								next(err);
-								return;
-						}
-						session.firstName = firstName;
-						session.onid = onid;
-					});
-				}
-				//TODO: change to a redirect instead of a render
-				let context = {};
-				context.firstName = session.firstName;
-				context.stylesheets = ['main.css', 'home.css'];
-				res.render('home', context);
+				//check if user with this onid exists
+				mysql.pool.query("SELECT * FROM `OSU_member` WHERE onid = '" + onid + "'", function(err, result){
+					if(err){
+						next(err);
+						return;
+					}
+					// if yes, log them in
+					else if (result.length > 0) {
+						session.onid = result[0].onid;
+						session.firstName = result[0].first_name;
+					}
+					//if no, add them to the database
+					else {
+						mysql.pool.query("INSERT INTO indaba_db.OSU_member (`onid`,`first_name`,`last_name`,`ONID_email`) VALUES (?,?,?,?)",
+						  [onid, firstName, lastName, email], function(err, result){
+							if(err){
+									next(err);
+									return;
+							}
+							session.firstName = firstName;
+							session.onid = onid;
+						});
+					}
+					//TODO: change to a redirect instead of a render
+					let context = {};
+					context.firstName = session.firstName;
+					context.stylesheets = ['main.css', 'home.css'];
+					res.render('home', context);
+				});
+			})
+			.catch(function (err) {
 			});
-
-		});
 	}
 })
-
-
-
-
-
 
 router.get('/home', function(req, res) {
 	let context = {};
