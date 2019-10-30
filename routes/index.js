0f81a326ef5a3524576d420a80ad2366b3f2aa47
Everett Williams <everettwilliams@localhost.localdomain>
2019-10-29 20:03:36 -0400

    remove test code from index

--- routes/index.js
+++ routes/index.js
@@ -89,35 +89,4 @@ router.get('/home', function(req, res) {
 	res.render('home', context);
 })
 
-router.get('/test_OSU_Users',function(req, res, next){
-  var context = {};
-  mysql.pool.query('SELECT * FROM indaba_db.OSU_member', function(err, rows, fields){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = JSON.stringify(rows);
-    res.send(context.results);
-  });
-});
-
-router.get('/insert_user',function(req,res,next){
-  var context = {};
-	var firstName = "Fred";
-	var lastName = "Sanford";
-	var email = "fred.sanford@watts.com";
-  mysql.pool.query("INSERT INTO indaba_db.OSU_member (`first_name`,`last_name`, `ONID_email`) VALUES (?,?,?)",
-      [firstName,
-       lastName,
-       email],
-       function(err, result){
-           if(err){
-               next(err);
-               return;
-         }
-         context.results = "Inserted OSU Member ";
-         res.send(context.results);
-     });
-});
-
 module.exports = router;
