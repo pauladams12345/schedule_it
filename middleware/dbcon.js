1919f85acd9d6e5c4753414dfcef37498ad4b59d
Paul Adams <pauladams12345@gmail.com>
2019-10-30 18:24:59 -0400

    Update references from mysql to mysql2

--- middleware/dbcon.js
+++ middleware/dbcon.js
@@ -1,4 +1,4 @@
-var mysql =       require('mysql'),
+var mysql =       require('mysql2'),
     session =     require('express-session'),
     MySQLStore =  require('express-mysql-session')(session);
 
