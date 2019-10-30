2d80a8b48eb594f8b512240890c3142c9e52ea56
pauladams12345 <pauladams12345@gmail.com>
2019-10-30 14:00:42 -0400

    Create experimental route

--- routes/index.js
+++ routes/index.js
@@ -82,7 +82,57 @@ router.get('/', function(req, res, next) {
 })
 
 
+// Experiment with promises
+router.get('/exp', function(req, res, next) {
 
+	// If there's no CAS ticket in the query string, render the landing page
+	if (!req.query.ticket) {
+		let context = {}
+		context.layout = 'no_navbar.handlebars'
+		context.stylesheets = ['main.css', 'login.css']
+		res.render('login', context);
+	}
+
+	// Otherwise, user has been redirected from CAS
+	// Validate ticket, redirect to personal homepage
+	else {
+		// Get ticket ID
+		let validation_ticket = req.query.ticket;
+
+		// Set up ticket validation request to CAS
+		let options = {
+			method: 'GET',
+			uri: 'https://login.oregonstate.edu/idp/profile/cas/serviceValidate',
+			headers: {
+				'Content-Type': 'text/xml'
+			},
+			qs: {
+				ticket: validation_ticket,
+				service: 'https://indaba-scheduler.herokuapp.com/'
+			}
+		};
+		//TODO: Break code below up into functions, maybe a module?
+		// Send validation request
+		request(options, function(err, response, body) {
+			//TODO: add error handling
+			// Parse successful request
+			let json = JSON.parse(parser.toJson(body));
+			let attributes = json['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
+			let onid = attributes['cas:uid'];
+			let firstName = attributes['cas:firstname'];
+			let lastName = attributes['cas:lastname'];
+			let fullName = attributes['cas:fullname'];
+			let email = attributes['cas:email'];
+
+			pool.promise().query("SELECT * FROM `OSU_member` WHERE onid = '" + onid + "'")
+				.then( ([rows,fields]) => {
+					console.log(rows);
+				})
+				.catch(console.log);
+
+		});
+	}
+})
 
 
 
