2b413da66241ebc61225df6043ac1d5fd1128910
Paul Adams <pauladams12345@gmail.com>
2019-10-30 18:55:02 -0400

    Update validation code to use promises via async/await

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,76 @@
 var Router = 	require('express-promise-router'),
-	router = 	new Router(),						//allows asynchronous route handlers
+	router = 	new Router(),						// allows asynchronous route handlers
 	parser =	require('xml2json'),
 	rp = 		require('request-promise-native'),
-	session = 	require('express-session');
+	session = 	require('express-session'),
+	user =		require('../models/user.js');
 
-// Display landing page or authenticate user and redirect
-router.get('/', async function(req, res, next) {
+// Redirects new arrivals to landing page. Handles authentication
+// for users redirected from CAS login then redirects to personal homepage
+router.get('/', async function (req, res, next) {
 
 	// If there's no CAS ticket in the query string, render the landing page
 	if (!req.query.ticket) {
-		let context = {}
-		context.layout = 'no_navbar.handlebars'
-		context.stylesheets = ['main.css', 'login.css']
-		res.render('login', context);
+		res.redirect('/login');
 	}
 
 	// Otherwise, user has been redirected from CAS
 	// Validate ticket, redirect to personal homepage
 	else {
-		// Get ticket ID
-		let validation_ticket = req.query.ticket;
 
-		// Set up ticket validation request to CAS
+		// Get ticket from query string
+		let cas_ticket = req.query.ticket;
+
+		// Validate ticket and get user's attributes
+		let attributes = await validateTicket(cas_ticket);
+
+		// Store user's name and onid in the session
+		req.session.onid = attributes.onid;
+		req.session.firstName = attributes.firstName;
+		
+		// If new user, store in database
+		await createUserIfNew(attributes);
+
+		// Render page
+		let context = {};
+		context.onid = attributes.onid;
+		context.firstName = attributes.firstName;
+		context.stylesheets = ['main.css', 'home.css'];
+		res.render('home', context);
+	}
+})
+
+// Displays user's personal homepage
+router.get('/home', async function (req, res, next) {
+	// If there is no session established, redirect to the landing page
+	if (!req.session.onid) {
+		res.redirect('../login');
+	}
+	// If there is a session, render user's homepage
+	else {
+		let context = {};
+		context.firstName = req.session.firstName;
+		context.stylesheets = ['main.css', 'login.css'];
+		res.render('home', context);
+	}
+
+});
+
+// Displays landing page
+//TODO: redirect to homepage if there's a session???
+router.get('/login', async function (req, res, next) {
+	let context = {};
+	context.layout = 'no_navbar.handlebars';
+	context.stylesheets = ['main.css', 'login.css'];
+	res.render('login.handlebars', context);
+});
+
+// Send validation request to CAS server with ticket, return attributes from response
+// TODO: handle errors
+async function validateTicket(cas_ticket){
+	try {
+
+		// Options for the CAS validation request
 		let options = {
 			method: 'GET',
 			uri: 'https://login.oregonstate.edu/idp/profile/cas/serviceValidate',
@@ -29,63 +78,45 @@ router.get('/', async function(req, res, next) {
 				'Content-Type': 'text/xml'
 			},
 			qs: {
-				ticket: validation_ticket,
+				ticket: cas_ticket,
 				service: 'https://indaba-scheduler.herokuapp.com/'
 			}
 		};
-		//TODO: Break code below up into functions, maybe a module?
-		// Send validation request
-		rp(options)
-			.then(function (body) {
-				//TODO: add error handling
-				// Parse successful request
-				let json = JSON.parse(parser.toJson(body));
-				let attributes = json['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
-				let onid = attributes['cas:uid'];
-				let firstName = attributes['cas:firstname'];
-				let lastName = attributes['cas:lastname'];
-				let fullName = attributes['cas:fullname'];
-				let email = attributes['cas:email'];
 
-				//check if user with this onid exists
-				mysql.pool.query("SELECT * FROM `OSU_member` WHERE onid = '" + onid + "'", function(err, result){
-					if(err){
-						next(err);
-						return;
-					}
-					// if yes, log them in
-					else if (result.length > 0) {
-						session.onid = result[0].onid;
-						session.firstName = result[0].first_name;
-					}
-					//if no, add them to the database
-					else {
-						mysql.pool.query("INSERT INTO indaba_db.OSU_member (`onid`,`first_name`,`last_name`,`ONID_email`) VALUES (?,?,?,?)",
-						  [onid, firstName, lastName, email], function(err, result){
-							if(err){
-									next(err);
-									return;
-							}
-							session.firstName = firstName;
-							session.onid = onid;
-						});
-					}
-					//TODO: change to a redirect instead of a render
-					let context = {};
-					context.firstName = session.firstName;
-					context.stylesheets = ['main.css', 'home.css'];
-					res.render('home', context);
-				});
-			})
-			.catch(function (err) {
-			});
+		// Validate ticket
+		const cas_info = await rp(options);
+
+		// Parse results from validation, converting from XML to JSON
+		let json = JSON.parse(parser.toJson(cas_info));
+		let cas_attributes = json['cas:serviceResponse']['cas:authenticationSuccess']['cas:attributes'];
+		
+		// Extract user's attributes
+		let attributes = {};
+		attributes.onid = cas_attributes['cas:uid'];
+		attributes.firstName = cas_attributes['cas:firstname'];
+		attributes.lastName = cas_attributes['cas:lastname'];
+		attributes.fullName = cas_attributes['cas:fullname'];
+		attributes.email = cas_attributes['cas:email'];
+
+		// Return user's attributes
+		return attributes;
+	} 
+	catch (err) {
+		console.log(err);
 	}
-})
+};
 
-router.get('/home', async function(req, res) {
-	let context = {};
-	context.stylesheets = ['main.css', 'home.css'];
-	res.render('home', context);
-})
+// Check if user exists in database. If not, create an entry.
+async function createUserIfNew(attributes){
+	try {
+		const [rows, fields] = await user.findUser(attributes.onid);
+		if (rows.length == 0) {
+			await user.createUser(attributes.onid, attributes.firstName, attributes.lastName, attributes.email);
+		}
+	}
+	catch (err){
+		console.log(err);
+	}
+}
 
 module.exports = router;
