206580553aeb6dbd1a3963f5a2c76fa8923aa971
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-02 15:21:27 -0400

    test

--- models/slot.js
+++ models/slot.js
@@ -7,14 +7,15 @@ module.exports.findUserSlots = async function(onid) {
 	try {
 		const connection = await sql.createConnection(dbcon);
 		const [rows, fields] = await connection.query(
-			"SELECT * FROM `Slot` s " +
+			"SELECT slot_id, DATE_FORMAT(slot_date, '%a %b %D %Y') slot_date, start_time, duration, slot_location," +
+			" event_name, description FROM `Slot` s " +
 			"INNER JOIN `Reserve_Slot` rs ON s.slot_id = rs.fk_slot_id " +
 			"RIGHT JOIN `Event` e ON s.fk_event_id = e.event_id " +
 			"WHERE fk_onid = ? " +
 			"ORDER BY s.slot_date",
 			[onid]);
 		return [rows, fields];
-	} 
+	}
 	catch (err) {
 		console.log(err);
 	}
@@ -26,7 +27,7 @@ module.exports.findSlot = async function(slotId) {
 		const connection = await sql.createConnection(dbcon);
 		const [rows, fields] = await connection.query("SELECT * FROM `Slot` WHERE slot_id = ?", [slotId]);
 		return [rows, fields];
-	} 
+	}
 	catch (err) {
 		console.log(err);
 	}
