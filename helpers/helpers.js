681c2f74279dc89195bd814eb4b787488284ffd7
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-02 15:07:28 -0400

    test

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -107,7 +107,7 @@ module.exports.processReservationsForDisplay = async function (reservations){
 
 		//  Create a nested  object for the current reservation
 		events[id].reservations[resv.slot_id] = {
-			date: resv.slot_date.toString().substring(0,16),		// example of how to store data
+			date: resv.slot_date.toString().substring(0,17),		// example of how to store data
 			time: resv.start_time,
 			location: resv.slot_location
 			 //Fill in the rest of the data needed here
