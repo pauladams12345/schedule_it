4a82a62dc19861b2a3563555031f6fb3ac7708c9
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-03 10:36:12 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,7 @@ router.get('/home', async function (req, res, next) {
 		let [reservations, fields] = await slot.findUserSlots(req.session.onid);
 
 		// Process response from database into a handlebars-friendly format
-		context.events = await helpers.processReservationsForDisplay(reservations);
+		context.events = await helpers.processReservationsForDisplay(reservations, req.session.onid);
 
 		context.firstName = req.session.firstName;
 		context.stylesheets = ['main.css', 'login.css'];
