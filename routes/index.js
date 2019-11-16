3a37551e07afc88fe8c6fab6e7cee430f3d0302f
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-16 17:20:14 -0500

    created jumbotron for manage event page

--- routes/index.js
+++ routes/index.js
@@ -103,7 +103,7 @@ router.get('/manage', async function (req, res, next) {
 	'@fullcalendar/timegrid/main.css', '@fullcalendar/bootstrap/main.css'];
 	context.scripts = ['calendar.js', 'create.js', '@fullcalendar/core/main.js', '@fullcalendar/daygrid/main.js',
 	'@fullcalendar/timegrid/main.js', '@fullcalendar/bootstrap/main.js', '@fullcalendar/interaction/main.js'];
-	res.render('create', context);
+	res.render('manage', context);
 });
 
 // Use this route to test locally without constantly re-deploying to Heroku
