4ece8974bd70895e5fce64a8da9263ba95fd31d0
pauladams12345 <pauladams12345@gmail.com>
2019-11-17 12:12:41 -0500

    format

--- models/slot.js
+++ models/slot.js
@@ -7,9 +7,9 @@ module.exports.findUserSlots = async function(onid) {
 	try {
 		const connection = await sql.createConnection(dbcon);
 		const [rows, fields] = await connection.query(
-			"SELECT slot_id, DATE_FORMAT(slot_date, '%a %b %D %Y') slot_date," +
-			"start_time, duration, slot_location," +
-			" event_name, description, event_id FROM `Slot` s " +
+			"SELECT slot_id, " //DATE_FORMAT(slot_date, '%a %b %D %Y') slot_date," +
+			"start_time, end_time, slot_location, " +
+			"event_name, description, event_id FROM `Slot` s " +
 			"INNER JOIN `Reserve_Slot` rs ON s.slot_id = rs.fk_slot_id " +
 			"RIGHT JOIN `Event` e ON s.fk_event_id = e.event_id " +
 			"WHERE fk_onid = ? " +
