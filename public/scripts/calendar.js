02df1559b4f3a806f2e8c7a1cb1ab24266ae36f9
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-17 09:35:52 -0500

    set create event page to redirect to manage

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -39,7 +39,6 @@ document.addEventListener('DOMContentLoaded', function() {
       var hours = parseInt(document.getElementById('defaultDurationHours').value, 10);
       var minutes = parseInt(document.getElementById('defaultDurationMinutes').value, 10);
       var duration = (60 * hours) + minutes;
-      console.log(duration);
       info.end.setTime(info.start.getTime() + duration * 60000);
       var calenderEvent = calendar.addEvent({id: slotId, start: info.start, end: info.end});
       appendSlot(info.startStr, info.endStr, slotId, calenderEvent, duration);//appendSlot(info.start, info.end, slotId, calenderEvent);
@@ -104,6 +103,7 @@ function appendSlot(startTime, endTime, slotId, calenderEvent, slotD) {
   duration.setAttribute('id', 'slotEnd' + slotId);
   duration.value = slotD;
   duration.hidden = false;
+  console.log(duration);
 
   // Slot location. Defaults to null. Will be replaced with defaultLocation
   // upon form submission if not explicitly specified.
