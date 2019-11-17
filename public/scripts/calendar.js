0e96293bc47a25a48005f9678b348885ac3f2209
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-17 10:14:39 -0500

    set create event page to redirect to manage

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var duration = (60 * hours) + minutes;
       info.end.setTime(info.start.getTime() + duration * 60000);
       var calenderEvent = calendar.addEvent({id: slotId, start: info.start, end: info.end});
-      appendSlot(info.startStr, info.endStr, slotId, calenderEvent, duration);//appendSlot(info.start, info.end, slotId, calenderEvent);
+      appendSlot(info.startStr, info.endStr, slotId, calenderEvent, 60);//appendSlot(info.start, info.end, slotId, calenderEvent);
     },
     // Upon clicking an existing slot, show the modal to edit details
     eventClick: function(clickInfo) {
