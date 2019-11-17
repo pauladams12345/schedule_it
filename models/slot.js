da452860c1d485f07a6607983f35e4c0b23c9309
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-16 20:18:27 -0500

    set create event page to redirect to manage

--- models/slot.js
+++ models/slot.js
@@ -68,7 +68,7 @@ module.exports.findSlotAttendees = async function(slotId) {
 //convert duration in minutes to hh:mm:ss for MySQL table
 durationTime = async function(duration){
 	try{
-		let slotDuration = await connection.query(SELECT SEC_TO_TIME(duration * 60));
+		let slotDuration = await connection.query("SELECT SEC_TO_TIME(" + duration + "* 60)");
 		connection.end();
 		return slotDuration;
 	}
