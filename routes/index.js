0bbde3d27b535ba6e1b925c1e1af799e3174e776
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-17 10:12:11 -0500

    set create event page to redirect to manage

--- routes/index.js
+++ routes/index.js
@@ -154,7 +154,7 @@ router.post('/create', async function (req, res, next) {
 	//parse slot date/time substring
 	for (let timeSlot of slotArray){
 		let dateTime = await helpers.parseDateTimeString(timeSlot);
-		await slot.createSlot(eventId, location, dateTime[0], dateTime[1], '01:00:00');
+		await slot.createSlot(eventId, location, dateTime[0], dateTime[1], duration);
 	}
 	res.redirect('/manage');
 });
