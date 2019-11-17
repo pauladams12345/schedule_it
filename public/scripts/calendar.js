7b9fea4fa5a81dd0e5276f432836480dadb66475
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-16 23:15:43 -0500

    set create event page to redirect to manage

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -98,8 +98,7 @@ function appendSlot(startTime, endTime, slotId, calenderEvent, slotDuration) {
 
   // Event duration (remains hidden)
   // Ensures only one input is created otherwise will have parsing issuses in index.js
-  var elementExists = document.getElementsByName('slotDuration');
-  if (typeof(elementExists) != 'undefined' && elementExists != null){
+  if (document.getElementsByName('slotDuration').length === 0){
     var duration = document.createElement('input');
     duration.setAttribute('type', 'text');
     duration.setAttribute('name', 'slotDuration');  //end.setAttribute('name', 'slot' + slotId);
