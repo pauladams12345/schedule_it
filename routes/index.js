fecbbb5ea623d71b9eee251915cbe912709b7f27
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-18 02:43:38 -0500

    added mysql query for manage page table

--- routes/index.js
+++ routes/index.js
@@ -100,6 +100,7 @@ router.get('/create', async function (req, res, next) {
 router.get('/manage', async function (req, res, next) {
 	let context = {};
 	let [reservations, fields] = await slot.eventSlotResv(173);
+	console.log(reservations);
 	context.table = await helpers.createTable(reservations);
 
 	context.stylesheets = ['main.css', 'login.css', '@fullcalendar/core/main.css', '@fullcalendar/daygrid/main.css',
