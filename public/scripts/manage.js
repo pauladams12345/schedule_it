7687e33961453f0e00839190ef56ef09e5e33655
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-18 01:47:09 -0500

    added mysql query for manage page table

--- public/scripts/manage.js
+++ public/scripts/manage.js
@@ -1,5 +1,21 @@
 //Pulls table with reservation info from database.
 //and calls function to create table on page.
-document.addEventListener('DOMContentLoaded', function() {
+
+let eventId = 173;
+console.log(eventId);
+
+let resvTable = async function(eventId){
+  try {
+    let table = await slot.eventSlotResv(eventId);
+    createResvTable(table);
+  }
+  catch (err) {
+    console.log(err);
+  }
+};
+
+function createResvTable(table){
   console.log("test");
-});
+};
+
+resvTable(eventId);
