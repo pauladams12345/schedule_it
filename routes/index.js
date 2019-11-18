9dcda233517c065935c1c5886598b4e64484dfa5
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-18 00:03:37 -0500

    added mysql query for manage page table

--- routes/index.js
+++ routes/index.js
@@ -101,7 +101,7 @@ router.get('/manage', async function (req, res, next) {
 	let context = {};
 	context.stylesheets = ['main.css', 'login.css', '@fullcalendar/core/main.css', '@fullcalendar/daygrid/main.css',
 	'@fullcalendar/timegrid/main.css', '@fullcalendar/bootstrap/main.css'];
-	context.scripts = ['calendar.js', 'create.js', '@fullcalendar/core/main.js', '@fullcalendar/daygrid/main.js',
+	context.scripts = ['calendar.js', 'manage.js', '@fullcalendar/core/main.js', '@fullcalendar/daygrid/main.js',
 	'@fullcalendar/timegrid/main.js', '@fullcalendar/bootstrap/main.js', '@fullcalendar/interaction/main.js'];
 	res.render('manage', context);
 });
