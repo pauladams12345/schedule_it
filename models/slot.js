96fcc5cd15bef21f0e7cf46ba178ed6da46a3f89
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-18 23:07:06 -0500

    added mysql query for manage page table

--- models/slot.js
+++ models/slot.js
@@ -69,7 +69,7 @@ module.exports.eventSlotResv = async function(eventId){
 	try{
 		const connection = await sql.createConnection(dbcon);
 		const [rows, fields] = await connection.query("SELECT first_name, last_name, " +
-		"ONID_email, slot_date, start_time, location FROM  `Event` " +
+		"ONID_email, DATE_FORMAT(slot_date, '%a %b %D %Y') slot_date, start_time, location FROM  `Event` " +
 		"INNER JOIN `Slot` ON fk_event_id = event_id INNER JOIN `Reserve_Slot` ON " +
 		"fk_slot_id = slot_id INNER JOIN `OSU_member` ON fk_onid = onid WHERE event_id = ? ORDER BY slot_date", [eventId]);
 		return [rows, fields];
