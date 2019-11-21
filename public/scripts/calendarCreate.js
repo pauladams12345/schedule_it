8b970086c3b6386d9c9820a338bcff3e1d9edd44
pauladams12345 <pauladams12345@gmail.com>
2019-11-21 18:06:57 -0500

    Get create working with new slotStates

--- public/scripts/calendarCreate.js
+++ public/scripts/calendarCreate.js
@@ -1,80 +1,245 @@
+var numEmails = 0;
+var slotIds = [];         // Store all id of every slot created
+var slotStates = {};      // Track state of each slot. Options: new, notUsed
+configureFormValidation();
+configureCalendar();
+
+// Disable form submissions if there are invalid fields
+// Adapted from https://getbootstrap.com/docs/4.0/components/forms/
+function configureFormValidation() {
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.classList.add('was-validated');
+        if (form.checkValidity() === true) {
+          var data = $(this).serialize();
+          data += "&" + $.param(slotStates);
+          data += "&slotIds=" + encodeURIComponent(slotIds);
+          console.log(data);  
+          $.ajax({
+            url : $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: data,
+            success: function(redirectURI) {
+               window.location.replace(redirectURI); 
+            },
+            error: function (jXHR, textStatus, errorThrown) {
+              alert(errorThrown);
+            }
+          });
+        }
+      }, false);
+    });
+  }, false);
+}
+
 // TODO: figure out how to handle slot ID assignment on Manage Event
 // Maybe pick a random starting value tied to the date/time? Want to
 // avoid overlap.
 //Script to create and manipulate the calendar on the create event page
-document.addEventListener('DOMContentLoaded', function() {
-  dateFormat = {}
-  var context = {};
-  var startTime;
-  var endTime;
-  var slotCounter = 0;
-  var max_attendees;
-  var calendarEl = document.getElementById('calendar');
-  var modal = document.getElementById('exampleModal');
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'bootstrap' ],
-    themeSystem: 'bootstrap',
-    defaultView: 'timeGridWeek',
-    slotDuration: '00:30',
-    snapDuration: '00:15',
-    //slotLabelInterval: '01:00:00',
-    editable: true,
-    selectable: true,
-    eventLimit : true,
-    longPressDelay: 20,
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-    },
-    eventTimeFormat: { // 07:30p.m.
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true
-    },
-    // Upon clicking an empty spot on calendar, create new slot using defaults
-    select: function(info) {
-      var slotId = slotCounter;
-      slotCounter++;
-      var hours = parseInt(document.getElementById('defaultDurationHours').value, 10);
-      var minutes = parseInt(document.getElementById('defaultDurationMinutes').value, 10);
-      var duration = (60 * hours) + minutes;
-      info.end.setTime(info.start.getTime() + duration * 60000);
-      var calenderEvent = calendar.addEvent({id: slotId, start: info.start, end: info.end});
-      appendSlot(info.start, info.end, slotId, calenderEvent);
-      let numSlots = document.getElementById('numSlots');
-      numSlots.value = parseInt(numSlots.value, 10) + 1;
-    },
-    // Upon clicking an existing slot, show the modal to edit details
-    eventClick: function(clickInfo) {
-      var slotId = clickInfo.event.id;
-      var slot = document.getElementById('slot' + slotId);
-      var location = document.getElementById('slotLocation' + slotId);
-      var maxAttendees = document.getElementById('slotMaxAttendees' + slotId);
-      location.placeholder = document.getElementById('defaultLocation').value;
-      maxAttendees.placeholder = document.getElementById('defaultMaxAttendees').value;
-      slot.hidden = false;
-      var saveButton = document.getElementById('modalSave');
-      saveButton.addEventListener('click', function(event) {
-        slot.hidden = true;
-      });
-      $('#addEventSlot').modal('show');
-    },
-    // Upon dragging and dropping an event, update the start and end times
-    eventDrop: function(dropInfo) {
-      var startInput = document.getElementById("slotStart" + dropInfo.event.id);
-      var endInput = document.getElementById("slotEnd" + dropInfo.event.id);
-      startInput.value = dropInfo.event.start;
-      endInput.value = dropInfo.event.end;
-    },
-    // Upon resizing an event, update the end time
-    eventResize: function(resizeInfo) {
-      var endInput = document.getElementById("slotEnd" + resizeInfo.event.id);
-      endInput.value = resizeInfo.event.end;
-    }
+function configureCalendar() {
+  document.addEventListener('DOMContentLoaded', function() {
+    dateFormat = {}
+    var context = {};
+    var startTime;
+    var endTime;
+    var slotCounter = 0;
+    var max_attendees;
+    var calendarEl = document.getElementById('calendar');
+    var modal = document.getElementById('exampleModal');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'bootstrap' ],
+      themeSystem: 'bootstrap',
+      defaultView: 'timeGridWeek',
+      slotDuration: '00:30',
+      snapDuration: '00:15',
+      //slotLabelInterval: '01:00:00',
+      editable: true,
+      selectable: true,
+      eventLimit : true,
+      longPressDelay: 20,
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+      },
+      eventTimeFormat: { // 07:30p.m.
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+      },
+      // Upon clicking an empty spot on calendar, create new slot using defaults
+      select: function(info) {
+        var slotId = slotCounter;
+        slotCounter++;
+        var hours = parseInt(document.getElementById('defaultDurationHours').value, 10);
+        var minutes = parseInt(document.getElementById('defaultDurationMinutes').value, 10);
+        var duration = (60 * hours) + minutes;
+        info.end.setTime(info.start.getTime() + duration * 60000);
+        var calenderEvent = calendar.addEvent({id: slotId, start: info.start, end: info.end});
+        appendSlot(info.start, info.end, slotId, calenderEvent);
+        slotStates["slotState" + slotId] = "new";
+        slotIds.push(slotId);
+      },
+      // Upon clicking an existing slot, show the modal to edit details
+      eventClick: function(clickInfo) {
+        var slotId = clickInfo.event.id;
+        var slot = document.getElementById('slot' + slotId);
+        var location = document.getElementById('slotLocation' + slotId);
+        var maxAttendees = document.getElementById('slotMaxAttendees' + slotId);
+        location.placeholder = document.getElementById('defaultLocation').value;
+        maxAttendees.placeholder = document.getElementById('defaultMaxAttendees').value;
+        slot.hidden = false;
+        var saveButton = document.getElementById('modalSave');
+        saveButton.addEventListener('click', function(event) {
+          slot.hidden = true;
+        });
+        $('#addEventSlot').modal('show');
+      },
+      // Upon dragging and dropping an event, update the start and end times
+      eventDrop: function(dropInfo) {
+        var startInput = document.getElementById("slotStart" + dropInfo.event.id);
+        var endInput = document.getElementById("slotEnd" + dropInfo.event.id);
+        startInput.value = dropInfo.event.start;
+        endInput.value = dropInfo.event.end;
+      },
+      // Upon resizing an event, update the end time
+      eventResize: function(resizeInfo) {
+        var endInput = document.getElementById("slotEnd" + resizeInfo.event.id);
+        endInput.value = resizeInfo.event.end;
+      }
+    });
+    calendar.render();
+    document.getElementById('addEmailsButton').addEventListener('click', splitEmails);
   });
-  calendar.render();
-});
+}
+
+// Extract email addresses from textarea, create a separate email input
+// for each, and do input validation
+function splitEmails(event) {
+
+  // Get emails from textarea
+  var allEmails = document.getElementById('emailAddresses').value;  // extract emails
+  if ( allEmails.length == 0 ) {
+    return;
+  }
+  var splitEmails = allEmails.split(/\s*,\s*|\s+/);     // split emails into separate strings
+  document.getElementById('emailAddresses').value = ''; // delete text from textarea
+
+  // Create an email input and delete button for each email address
+  for (var email of splitEmails) {
+
+    if (email == "") {
+      continue;
+    }
+
+    numEmails++;
+    var newInput = createEmailInput(email, numEmails)   // create input
+    var newDeleteButton = createDeleteButton();         // create delete button
+    bindDelete(newInput, newDeleteButton);              // bind delete button to input
+
+    if ( newInput.checkValidity() ) {                   // do initial input validation
+      newInput.classList.add('is-valid');
+    } else {
+      newInput.classList.add('is-invalid');
+    }
+
+    newInput.addEventListener('blur', validateEmail);   // do subsequent validation on blur
+
+    appendInputAndButton(newInput, newDeleteButton);    // add to document
+
+  }
+};
+
+// Return an email input with value matching the 1st argument
+function createEmailInput(email, inputNumber) {
+  var input = document.createElement("input");
+
+  input.setAttribute('type', 'email');
+  input.setAttribute('class', 'form-control w-100');
+  input.setAttribute('name', 'emails');
+  input.setAttribute('id', 'email' + inputNumber);
+
+  input.value = email;
+
+  return input;
+};
+
+// Returns a button for deleting an email input
+function createDeleteButton() {
+  var button = document.createElement("button");
+
+  button.setAttribute('type', 'button');
+  button.setAttribute('class', 'btn btn-sm btn-danger mx-2');
+  button.setAttribute('id', 'emailDelete' + numEmails);
+  button.textContent = 'Delete';
+
+  return button;
+};
+
+// Checks if an email input's value is valid and sets formatting appropriately
+function validateEmail(event) {
+    // Add dynamic input validation
+  if ( this.checkValidity() ) {
+    this.classList.remove('is-invalid');
+    this.classList.add('is-valid');
+  }
+  else {
+    this.classList.add('is-valid');
+    this.classList.add('is-invalid');
+  }
+}
+
+// Appends the given email input and button to the document
+function appendInputAndButton(input, button) {
+    // Append row and column for new input and append to document
+    var row = document.createElement("div");
+    row.setAttribute('class', 'row')
+    var col = document.createElement("div");
+    col.setAttribute('class', 'col-12 form-inline d-flex');
+    var div = document.createElement("div");
+    div.setAttribute('style', 'display: flex; flex-grow: 1;')
+
+    div.appendChild(input)
+    col.appendChild(div);
+    col.appendChild(button);
+    row.appendChild(col);
+    document.getElementById('emailsDiv').appendChild(row);
+};
+
+// Sets the supplied button to delete the corresponding
+// input (and itself) upon being clicked
+function bindDelete(input, button) {
+  button.addEventListener('click', function(event) {
+    input.remove();
+    button.remove();
+  });
+};
+
+// Configure form submission
+function configureFormSubmission() {
+  // Submit name form with ajax to prevent page refresh
+  $('#createEventForm').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      url : $(this).attr('action'),
+      type: $(this).attr('method'),
+      data: $(this).serialize(),
+      success: function(data) {
+         console.log(data);
+      },
+      error: function (jXHR, textStatus, errorThrown) {
+        alert(errorThrown);
+      }
+    });
+  });
+
+};
 
 // Creates inputs for start time, end time, location, and maxAttendees
 // for a new slot and appends to the modal. Hidden by default.
@@ -136,8 +301,7 @@ function appendSlot(startTime, endTime, slotId, calenderEvent) {
   deleteButton.addEventListener('click', function(event) {
     calenderEvent.remove();
     slot.parentNode.removeChild(slot);
-    let numSlots = document.getElementById('numSlots');
-    numSlots.value = parseInt(numSlots.value, 10) - 1;
+    slotStates["slotState" + slotId] = "notUsed";
     $('#addEventSlot').modal('hide');
   });
 
--- routes/create.js
+++ routes/create.js
@@ -31,6 +31,7 @@ router.get('/create-test', async function (req, res, next) {
 
 // Process event creation form
 router.post('/create', async function (req, res, next) {
+
 	// Get values from request
 	let context = {};
 	let slotArray = [];
@@ -41,7 +42,12 @@ router.post('/create', async function (req, res, next) {
 		description = req.body.description,
 		visibility = req.body.attendeeNameVisibility,
 		emails = req.body.emails,
-		numSlots = req.body.numSlots;
+		numSlots = req.body.numSlots,
+		slotIds = req.body.slotIds;
+	console.log(slotIds);
+	if (slotIds.length > 0) {
+		slotIds = slotIds.split(',');
+	}
 
 	// Handle edge cases of 1 or 0 emails, convert to an array
 	if (typeof emails === 'string') {
@@ -60,23 +66,20 @@ router.post('/create', async function (req, res, next) {
 	// Store the invitations (users emails) in the database
 	await invitation.createInvitations(eventId, emails);
 
-
-	// // TODO: handle time zone conversions
 	// Process all slots
-	let i = 0;
-	let processedSlots = 0;
-	while (processedSlots < numSlots) {
-		if (req.body['slotStart' + i]) {
-			let start = new Date(req.body['slotStart' + i]);	// start date/time
-			let end = new Date(req.body['slotEnd' + i]);		// end date/time
+	for (let id of slotIds) {
+		if (req.body['slotState' + id] == 'new') {
+			
+			let start = new Date(req.body['slotStart' + id]);	// start date/time
+			let end = new Date(req.body['slotEnd' + id]);		// end date/time
 			let duration = (end - start) / 60000;				// duration in minutes
-			let location = req.body['slotLocation' + i];		// location
+			let location = req.body['slotLocation' + id];		// location
 
 			if (location == '') {								// if not specified, use default
 				location = defaultLocation;
 			}
 
-			let maxAttendees = req.body['slotMaxAttendees' + i];// maximum number of attendees
+			let maxAttendees = req.body['slotMaxAttendees' + id];// maximum number of attendees
 			if (maxAttendees == '') {							// if not specified, use default
 				maxAttendees = defaultMaxAttendees;
 			}
@@ -84,12 +87,9 @@ router.post('/create', async function (req, res, next) {
 			let [date, time] = await helpers.parseDateTimeString(start);	//convert start date/time to MySQL-compatible format
 
 			await slot.createSlot(eventId, location, date, time, duration, maxAttendees);	// Store slots in database
-
-			processedSlots++;
 		}
-		i++;
 	}
-	res.redirect('/manage/' + eventId) + "/";
+	res.send('/manage/' + eventId);
 });
 
 module.exports = router;
