d737cad380e05a889e867f4d4b7d739f73f6c618
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-23 16:02:46 -0500

    make a reservation page

--- routes/makeReservations.js
+++ routes/makeReservations.js
@@ -19,13 +19,13 @@ router.get('/make-reservations/:eventId', async function (req, res, next) {
 		let context = {};
 		eventId = req.params.eventId;
 
-		context.eventsManaging = await createsEvent.getUserEvents(req.session.onid);
+		context.eventDetails = await event.findEvent(eventId);
 
 		// Find all slots a user registered for in the past
-		let [reservations, fields] = await slot.findPastUserSlots(req.session.onid);
+		//let [reservations, fields] = await slot.findPastUserSlots(req.session.onid);
 
 		// Process response from database into a handlebars-friendly format
-		context.eventsAttending = await helpers.processReservationsForDisplay(reservations, req.session.onid);
+		//context.eventsAttending = await helpers.processReservationsForDisplay(reservations, req.session.onid);
 
 		context.stylesheets = ['main.css', 'home.css']
 		res.render('make-reservations', context);
