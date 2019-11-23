43bbcd07b75ae1d3e1371a533a8b546048c643a7
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-23 13:04:40 -0500

    make a reservation page

--- routes/makeReservations.js
+++ routes/makeReservations.js
@@ -17,22 +17,12 @@ router.get('/make-reservations', async function (req, res, next) {
 	// If there is a session, render users past reservations
 	else {
 		let context = {};
-
-		//context.eventsManaging = await createsEvent.getUserEvents(req.session.onid);
-
-		// Find all slots a user registered for in the past
-		//let [reservations, fields] = await slot.findPastUserSlots(req.session.onid);
-
-		// Process response from database into a handlebars-friendly format
-		//context.eventsAttending = await helpers.processReservationsForDisplay(reservations, req.session.onid);
-
-		//context.stylesheets = ['main.css', 'home.css']
-		//res.render('past-reservations', context);
     context.stylesheets = ['main.css', 'calendar.css', '@fullcalendar/core/main.css', '@fullcalendar/daygrid/main.css',
     '@fullcalendar/timegrid/main.css', '@fullcalendar/bootstrap/main.css'];
-    context.scripts = ['makeReservations.js', '@fullcalendar/core/main.js', '@fullcalendar/daygrid/main.js',
+    context.scripts = ['@fullcalendar/core/main.js', '@fullcalendar/daygrid/main.js',
     '@fullcalendar/timegrid/main.js', '@fullcalendar/bootstrap/main.js', '@fullcalendar/interaction/main.js'];
-    res.render('make-reservations', context);
+    //res.render('make-reservations', context);
+		res.send('success');
 	}
 });
 
