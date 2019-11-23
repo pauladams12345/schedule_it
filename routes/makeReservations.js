a739670288b49a6ee526ce84ee50bdc646d4cc89
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-23 13:12:16 -0500

    make a reservation page

--- routes/makeReservations.js
+++ routes/makeReservations.js
@@ -7,8 +7,13 @@ var Router = 		require('express-promise-router'),
 	createsEvent =	require('../models/createsEvent.js');
 	helpers = 		require('../helpers/helpers.js');
 
-router.post('/make-reservations', async function (req, res, next) {
-	res.send('success');
-});
+	router.get('/make-reservations', async function (req, res, next) {
+		let context = {};
+		context.stylesheets = ['main.css', 'calendar.css', '@fullcalendar/core/main.css', '@fullcalendar/daygrid/main.css',
+		'@fullcalendar/timegrid/main.css', '@fullcalendar/bootstrap/main.css'];
+		context.scripts = ['calendarCreate.js', '@fullcalendar/core/main.js', '@fullcalendar/daygrid/main.js',
+		'@fullcalendar/timegrid/main.js', '@fullcalendar/bootstrap/main.js', '@fullcalendar/interaction/main.js'];
+		res.render('create', context);
+	});
 
 module.exports = router;
