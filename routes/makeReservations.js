f6b3eb1c5bc0b6851f822d4cca754963e0a75dd3
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-23 13:08:45 -0500

    make a reservation page

--- routes/makeReservations.js
+++ routes/makeReservations.js
@@ -8,22 +8,7 @@ var Router = 		require('express-promise-router'),
 	helpers = 		require('../helpers/helpers.js');
 
 router.get('/make-reservations', async function (req, res, next) {
-
-	// If there is no session established, redirect to the landing page
-	if (!req.session.onid) {
-		res.redirect('../login');
-	}
-
-	// If there is a session, render users past reservations
-	else {
-		let context = {};
-    context.stylesheets = ['main.css', 'calendar.css', '@fullcalendar/core/main.css', '@fullcalendar/daygrid/main.css',
-    '@fullcalendar/timegrid/main.css', '@fullcalendar/bootstrap/main.css'];
-    context.scripts = ['@fullcalendar/core/main.js', '@fullcalendar/daygrid/main.js',
-    '@fullcalendar/timegrid/main.js', '@fullcalendar/bootstrap/main.js', '@fullcalendar/interaction/main.js'];
-    //res.render('make-reservations', context);
-		res.send('success');
-	}
+	res.send('success');
 });
 
 module.exports = router;
