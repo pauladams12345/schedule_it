b10b0a15b9506fef5e3d1851fee5c4d964e33f75
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-23 20:52:41 -0500

    make a reservation page

--- public/scripts/calendarReservation.js
+++ public/scripts/calendarReservation.js
@@ -2,7 +2,6 @@ var numEmails = 0;        // Used to create unique id for email inputs as they'r
 var slotIds = [];         // Store all id of every slot created
 var slotStates = {};      // Track state of each slot. Options: new, notUsed, existingUnmodified, existingModified, existingDeleted
 configureCalendar();
-console.log(document.getElementById('existingSlots')[0]);
 //configureFormSubmissions();
 //configureReservations();
 //configureFormValidation();
@@ -85,6 +84,14 @@ function configureCalendar() {
     });
     calendar.render();
   });
+  var existingSlots = document.getElementById('existingSlot');
+  for (var i = 0; i < existingSlots.length; i++) {
+    var slotId = existingSlots[i].getAttribute('id').substring(4);
+    var startTime = new Date(document.getElementById('slotStart' + slotId).value);
+    var endTime = new Date(document.getElementById('slotEnd' + slotId).value);
+    var location = document.getElementById('slotLocation' + slotId).value;
+    var calendarEvent = calendar.addEvent({id: slotId, start: startTime, end: endTime, title: location});
+  }
 }
 /*
 // When focus blurs from the default location input, update the title of all events
