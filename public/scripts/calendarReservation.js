743c113681c8ba202cf0011e442f2cbaf5622ff1
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-23 20:47:06 -0500

    make a reservation page

--- public/scripts/calendarReservation.js
+++ public/scripts/calendarReservation.js
@@ -2,7 +2,7 @@ var numEmails = 0;        // Used to create unique id for email inputs as they'r
 var slotIds = [];         // Store all id of every slot created
 var slotStates = {};      // Track state of each slot. Options: new, notUsed, existingUnmodified, existingModified, existingDeleted
 configureCalendar();
-console.log(document.getElementById('existingSlot'));
+console.log(document.getElementById('existingSlots'));
 //configureFormSubmissions();
 //configureReservations();
 //configureFormValidation();
