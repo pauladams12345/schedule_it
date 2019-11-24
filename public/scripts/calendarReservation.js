cdd680f9dd86f88bb7fc3a459b4a49e2f224c422
Everett Williams <everettwilliams@localhost.localdomain>
2019-11-24 16:23:54 -0500

    make a reservation page

--- public/scripts/calendarReservation.js
+++ public/scripts/calendarReservation.js
@@ -88,7 +88,7 @@ function configureCalendar() {
       var startTime = new Date(document.getElementById('slotStart' + slotId).value);
       var endTime = new Date(document.getElementById('slotEnd' + slotId).value);
       var location = document.getElementById('slotLocation' + slotId).value;
-      console.log(slotId);
+      console.log(startTime);
       var calendarEvent = calendar.addEvent({id: slotId, start: startTime, end: endTime, title: location});
     }
     calendar.render();
