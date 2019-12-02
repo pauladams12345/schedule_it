382a1d6589930b7de341600dd2306323cf370af0
Everett Williams <everettwilliams@localhost.localdomain>
2019-12-01 23:53:11 -0500

    add slot restrictions

--- routes/makeReservations.js
+++ routes/makeReservations.js
@@ -24,7 +24,6 @@ router.get('/make-reservations/:eventId', async function (req, res, next) {
 
 		context.eventDetails = await event.findEvent(eventId);
 		context.eventCreator = await event.getEventCreator(eventId);
-		let [numOfUserResv4Event, fields] = await event.getNumOfUserResv4Event(onid, eventId);
 		context.numOfUserResv4Event = await event.getNumOfUserResv4Event(onid, eventId);
 		let [slots, field] = await slot.findUserSlots(onid);
 		let eventSlots = await slot.findEventSlots(eventId);
