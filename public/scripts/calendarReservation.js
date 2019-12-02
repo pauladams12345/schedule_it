5b84f8b7f7c0baad8da8d113b863ff5e5266d64d
Everett Williams <everettwilliams@localhost.localdomain>
2019-12-02 00:22:22 -0500

    add slot restrictions

--- public/scripts/calendarReservation.js
+++ public/scripts/calendarReservation.js
@@ -45,6 +45,7 @@ function configureCalendar() {
         console.log(slotAttendee.length);
         console.log(numCurSelectedSlots);
         console.log(numUserResv);
+        console.log(max_resv_per_attendee);
         //logic for use cases: #resv per slot exceeded, #resv per event exceeded, or
         //limitations not exceeded.
         if((numCurSelectedSlots + numUserResv) >= max_resv_per_attendee){
