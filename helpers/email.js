2e75fc6695db3d069786aa569beada0312969da9
Paul Adams <pauladams12345@gmail.com>
2019-12-05 15:02:00 -0500

    Customize email

--- helpers/email.js
+++ helpers/email.js
@@ -3,59 +3,39 @@
 // SMTP server or create one in node.js
 
 var helper = require('sendgrid').mail;
-var from_email = new helper.Email('noreply@indaba-scheduler.herokuapp.com');
-var to_email = new helper.Email('adamspa@oregonstate.edu');
-var subject = 'Hello World from the SendGrid Node.js Library!';
-var content = new helper.Content('text/plain', 'Hello, Email!');
-var mail = new helper.Mail(from_email, subject, to_email, content);
-
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON(),
-});
-
 
 // Send an invitation email with the specified info to all email addresses in the
 // emails array using mailgun.
 module.exports.sendInvitationEmail = function(organizerName, eventName, eventDescription, eventId, emails) {
 
+	messageText = "Hello!\n\n" + 
+	organizerName + " has inivted you to the following event: " +
+	eventName + "\n";
+	if (eventDescription) {
+		messageText += "Event description: " + eventDescription + "\n\n";
+	}
+	messageText += "Click below to check out the event details and make your reservation\n\n" +
+	"https://indaba-scheduler.herokuapp.com/make-reservations/" + eventId;
+
+	var fromEmail = new helper.Email({
+		name: 'Indaba Scheduler',
+		email: 'noreply@indaba-scheduler.herokuapp.com'
+	});
+	var toEmail = new helper.Email(emails);
+	var subject = 'Invitation to ' + eventName;
+	var content = new helper.Content('text/plain', messageText);
+	var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+	var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+	var request = sg.emptyRequest({
+	  method: 'POST',
+	  path: '/v3/mail/send',
+	  body: mail.toJSON(),
+	});
 
 	sg.API(request, function(error, response) {
 	  console.log(response.statusCode);
 	  console.log(response.body);
 	  console.log(response.headers);
 	});
-
-
-
-
-	// let from = "Indaba Scheduler <noreply@indaba-scheduler.herokuapp.com>";
-	// let recipientVariables = {}
-	// for (let email of emails) {
-	// 	recipientVariables[email] = {};
-	// }
-	// let subject = "Invitation to " + eventName;
-	
-	// let text = "Hello!\n\n" + 
-	// organizerName + " has inivted you to the following event: " +
-	// eventName + "\n";
-	// if (eventDescription) {
-	// 	text += "Event description: " + eventDescription + "\n\n";
-	// }
-	// text += "Click below to check out the event details and make your reservation\n\n" +
-	// "https://indaba-scheduler.herokuapp.com/make-reservations/" + eventId;
-
-	// const data = {
-	// 	"from": from,
-	// 	"to": emails,
-	// 	"recipient-variables": recipientVariables,
-	// 	"subject": subject,
-	// 	"text": text
-	// };
-
-	// mg.messages().send(data, function (error, body) {
-	// 	console.log(body);
-	// });	
 };
