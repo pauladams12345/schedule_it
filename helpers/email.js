495292b64491e01f1a0b7dd898e489c56384792b
Paul Adams <pauladams12345@gmail.com>
2019-12-05 15:25:19 -0500

    Switch to modern sendgrid packages

--- helpers/email.js
+++ helpers/email.js
@@ -2,7 +2,8 @@
 // TODO: During migration to OSU servers, either get set up with university
 // SMTP server or create one in node.js
 
-var helper = require('sendgrid').mail;
+let sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Send an invitation email with the specified info to all email addresses in the
 // emails array using mailgun.
@@ -17,25 +18,15 @@ module.exports.sendInvitationEmail = function(organizerName, eventName, eventDes
 	messageText += "Click below to check out the event details and make your reservation\n\n" +
 	"https://indaba-scheduler.herokuapp.com/make-reservations/" + eventId;
 
-	var fromEmail = new helper.Email({
-		'name': 'Indaba Scheduler',
-		'email': 'noreply@indaba-scheduler.herokuapp.com'
-	});
-	var toEmail = new helper.Email(emails);
-	var subject = 'Invitation to ' + eventName;
-	var content = new helper.Content('text/plain', messageText);
-	var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+	let msg = {
+		to: emails,
+		from: {
+			'name': 'Indaba Scheduler',
+			'email': 'noreply@indaba-scheduler.herokuapp.com'
+		},
+		subject: 'Invitation to ' + eventName,
+		text: messageText
+	}
 
-	var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-	var request = sg.emptyRequest({
-	  method: 'POST',
-	  path: '/v3/mail/send',
-	  body: mail.toJSON(),
-	});
-
-	sg.API(request, function(error, response) {
-	  console.log(response.statusCode);
-	  console.log(response.body);
-	  console.log(response.headers);
-	});
+	sgMail.sendMultiple(msg);
 };
